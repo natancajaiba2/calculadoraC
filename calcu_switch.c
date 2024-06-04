0cdb9175950aebd541368227049e9a9d1720eb3b
natan santos <natan.cajaiba@etec.sp.gov.br>
2024-06-04 19:45:16 -0300

    calculadora em C feito com switch

---
+++ calcu_switch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	float num1;
+	float num2;
+	float resultado;
+
+	printf("digite o primeiro numero");
+	scanf ("%f" &num1);
+        prinf("digie o segundo numero");
+	scanf("%f") &num2;
+
+switch(operacao){
+	case '+':
+		resultado = num1 + num2;
+		printf("resultado %.2f\n", resultado);
+		break;
+	case '-':
+		resultado = num1 - num2;
+		printf("resultado %.2f\n", resultado);
+		break;
+	case '*':
+		resultado = num1 * num2*;
+		printf("resultado %.2f\n", resultado);
+		break;
+	case '/':
+		resultado = num1 / num2;
+		printf("resultado %.2f\n", resultado);
+		break;
+
+			
+
+
+}	
+
+
+
+
+}
